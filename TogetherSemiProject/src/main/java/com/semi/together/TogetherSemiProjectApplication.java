f77411adf49789f9466c91d46fcf5c5d33737dd3
Jun <Jun@LAPTOP-U827OS29>
2023-05-02 09:51:27 +0900

    test

---
+++ TogetherSemiProject/src/main/java/com/semi/together/TogetherSemiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.semi.together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherSemiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherSemiProjectApplication.class, args);
+	}
+
+}
---
+++ TogetherSemiProject/src/test/java/com/semi/together/TogetherSemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semi.together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherSemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

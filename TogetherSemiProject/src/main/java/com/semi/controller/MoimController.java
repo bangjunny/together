e8563623a4fe410947bd62c4a22dedd65f62d37b
Park Myungsoo <qkautnkr@gmail.com>
2023-05-03 19:59:49 +0900

    0503 pms commit

--- TogetherSemiProject/src/main/java/com/semi/controller/MoimController.java
+++ TogetherSemiProject/src/main/java/com/semi/controller/MoimController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.semi.dto.MoimDto;
+import com.semi.mapper.MoimMapper;
 import com.semi.service.MoimService;
 
 import naver.cloud.NcpObjectStorageService;
@@ -15,11 +18,17 @@ import naver.cloud.NcpObjectStorageService;
 @RequestMapping("/moim")
 public class MoimController {
    
+	@Autowired
+	MoimMapper moimMapper;
+	
+	//버켓이름지정
+	private String bucketName="together-bucket-104";
+		
+	@Autowired
+	private NcpObjectStorageService storageService;
+	
    @Autowired
    private MoimService moimService;
-   
-   @Autowired
-   private NcpObjectStorageService storageService;
    
    @GetMapping("/moimlist")
    private String moimlist()
@@ -46,5 +55,18 @@ public class MoimController {
 	   return "/main/moim/moimform";
    }
    
+   @PostMapping("/insert")
+	public String insert(MoimDto dto,MultipartFile upload)
+	{
+		//네이버 클라우드의 버켓에 사진 업로드하기
+		String photo=storageService.uploadFile(bucketName, "moim", upload);
+		//반환된 암호화된 파일명을 dto에 넣기
+		dto.setMphoto(photo);
+		
+		//db insert
+		moimMapper.insertMoim(dto);
+		
+		return "redirect:moimlist";
+	}
 }
 
